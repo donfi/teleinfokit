719ca90b0ad9e714a9a02edfd351cad1cc091f15
Oliver Haskell <oliver.haskell@faria.edu>
2026-02-02 11:08:48 +0100

    Utiliser def_ent_id dans MQTT Discovery

--- include/version.h
+++ include/version.h
@@ -1,7 +1,7 @@
 //  DO NOT EDIT MANUALLY THIS FILE - IT IS DELETED AND RECREATED AT EACH BUILD !!!! 
 #ifndef BUILD_HASH
-  #define BUILD_HASH "390806"
+  #define BUILD_HASH "c9d7d9"
 #endif
 #ifndef VERSION
-  #define VERSION "v2.1.0.390806"
+  #define VERSION "v2.1.0.c9d7d9"
 #endif
